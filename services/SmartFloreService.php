11a0737c1d0dbd0569fd105a1354e36339672de8
Aurélien Peronnet <aurelienperonnet@gmail.com>
2015-06-08 15:38:59 +0200

    meilleure gestion des actions en cas de redirection

--- services/SmartFloreService.php
+++ services/SmartFloreService.php
@@ -55,6 +55,8 @@ class SmartFloreService {
 	// ---------------------------------------------------------------------------------------------
 	function init() {
 		$methode = $_SERVER['REQUEST_METHOD'];
+		// Certaines version de php remplissent PATH_INFO et d'autres ORIG_PATH_INFO en cas de redirection
+		$path_info = !empty($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : (!empty($_SERVER['ORIG_PATH_INFO']) ? $_SERVER['ORIG_PATH_INFO'] : '');
 		$requete = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
 
 		// Récupération d'un éventuel jeton SSO
