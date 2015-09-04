f4f49b3d2d35c0ef4c30c51026bfd9cd4fbbb49d
Mathias Chouet <mathias@tela-botanica.org>
2015-09-04 11:28:16 +0200

    Boutons remplacés par des liens, sauf "créer / éditer fiche" et "supprimer fiche"

--- js/services/liensService.js
+++ js/services/liensService.js
@@ -2,30 +2,26 @@ smartFormApp.service('liensService', function() {
 	
 	var liensService = {};
 	
-	liensService.ouvrirFicheMobile = function(fiche) {
+	liensService.lienFicheMobile = function(fiche) {
 		referentiel_fiche = ""+fiche.infos_taxon.referentiel;
 		url_fiche = config.url_fiche_mobile.replace('{referentiel}', referentiel_fiche.toLowerCase());
 		url_fiche = url_fiche.replace('{num_nom}', fiche.infos_taxon.num_nom);
-		console.log(url_fiche);
-		window.open(url_fiche);
+		//console.log(url_fiche);
+		return url_fiche;
 	};
 	
-	liensService.ouvrirQrCode = function(fiche) {
+	liensService.lienQrCode = function(fiche) {
 		url = config.url_qr_code.replace('{referentiel}', fiche.infos_taxon.referentiel.toLowerCase());
 		url = url.replace('{num_nom}', fiche.infos_taxon.num_nom);
-		window.open(url);
+		return url;
 	};
-	
-	liensService.ouvrirPageInscription = function() {
-		window.open(config.url_inscription);
-	};
-	
+
 	liensService.exporterFicheEnPdf = function(fiche, sentierTitre) {
 		url = config.url_service_export.replace('{referentiel}', fiche.infos_taxon.referentiel.toLowerCase());
 		url = url.replace('{type}', 'fiche');
 		url = url.replace('{num_tax}', fiche.infos_taxon.num_taxonomique);
 		url = url.replace('{sentierTitre}', sentierTitre);
-		window.open(url);
+		return url;
 	};
 		
 	liensService.exporterSentierEnPdf = function(sentierTitre) {
@@ -33,7 +29,7 @@ smartFormApp.service('liensService', function() {
 		url = url.replace('{type}', 'sentier');
 		url = url.replace('{num_tax}', '');
 		url = url.replace('{sentierTitre}', sentierTitre);
-		window.open(url);
+		return url;
 	};
 	
 	return liensService;	
