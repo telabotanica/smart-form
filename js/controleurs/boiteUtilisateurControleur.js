55a5e535b8507a9f2fc0d8e1105e74214979f6c5
Mathias Chouet <mathias@tela-botanica.org>
2015-09-04 16:53:18 +0200

    Rafraîchissement automatique du jeton SSO

--- js/controleurs/boiteUtilisateurControleur.js
+++ js/controleurs/boiteUtilisateurControleur.js
@@ -1,4 +1,4 @@
-smartFormApp.controller('BoiteUtilisateurControleur', function ($scope, $rootScope, etatApplicationService, liensService) {
+smartFormApp.controller('BoiteUtilisateurControleur', function ($scope, $rootScope, $interval, etatApplicationService) {
 	
 	this.utilisateur = {};
 	this.urlInscription = config.url_inscription;
@@ -7,6 +7,9 @@ smartFormApp.controller('BoiteUtilisateurControleur', function ($scope, $rootSco
 	this.erreur = false;
 	this.courrielOK = false;
 	this.mdpOK = false;
+
+	// rafraîchissement périodique du jeton
+	this.timer = null;
 	
 	// au cas où autre chose nous aurait connecté dans l'application
 	$scope.$on('utilisateur.utilisateur-connecte', function(event, utilisateur) {
@@ -69,26 +72,49 @@ smartFormApp.controller('BoiteUtilisateurControleur', function ($scope, $rootSco
 		lthis.message = "";
 		this.erreur = false;
 	};
-	
-	this.connaitreEtatUtilisateur = function() {
+
+	/**
+	 * Appelle l'annuaire pour connaître l'état de l'utilisateur; si celui-ci
+	 * est connecté, rappelle l'annuaire avant l'expiration de jeton pour éviter
+	 * cette dernière; si toutRecharger est false, ne fera que rafraîchir le
+	 * jeton sans recharger les infos utilisateur (favoris, sentiers etc.)
+	 */
+	this.connaitreEtatUtilisateur = function(toutRecharger) {
+		if (toutRecharger === undefined) {
+			toutRecharger = true;
+		}
 		etatApplicationService.connaitreEtatUtilisateur( 
 		function(data) {
 			if(!!data && !!data.session) {
 				// infos SSO
 				etatApplicationService.jeton = data.token; // jeton rafraîchi
 				etatApplicationService.dureeJeton = data.duration;
-				// infos utilisateur
-				lthis.utilisateur = lthis.construireUtilisateurDepuisJeton(data.token);
-				lthis.utilisateur.connecte = true;
-				etatApplicationService.utilisateur = lthis.utilisateur;				
-				$rootScope.$broadcast('utilisateur.utilisateur-connecte', etatApplicationService.utilisateur);
+				// infos utilisateur, si on le force ou si on n'était pas connecté
+				if (toutRecharger || ! lthis.utilisateur.connecte) {
+					lthis.utilisateur = lthis.construireUtilisateurDepuisJeton(data.token);
+					lthis.utilisateur.connecte = true;
+					etatApplicationService.utilisateur = lthis.utilisateur;				
+					$rootScope.$broadcast('utilisateur.utilisateur-connecte', etatApplicationService.utilisateur);
+				}
+				// rafraîchissement automatique du jeton
+				if (lthis.timer == null) {
+					// petite précaution de 10% pour ne pas se faire biaiser
+					var delai = Math.floor(etatApplicationService.dureeJeton * 0.9) * 1000;
+					lthis.timer = $interval(function() {
+						//console.log("C'est reparti pour un tour, Georgette !");
+						lthis.connaitreEtatUtilisateur(false);
+					}, delai);
+				}
 			}
 		},
 		function(data) {
 			
 		});
 	};
-	
+
+	// déconnecte l'utilisateur et arrête le rafraîchissement du jeton
+	// @TODO - devrait-on plutôt l'accélérer pour détecter une connexion depuis
+	// un autre endroit ?
 	this.deconnecterUtilisateur = function() {
 		etatApplicationService.deconnecterUtilisateur( 
 		function(data) {
@@ -99,9 +125,12 @@ smartFormApp.controller('BoiteUtilisateurControleur', function ($scope, $rootSco
 			lthis.initialiserUtilisateurVide();
 			etatApplicationService.utilisateur = lthis.utilisateur;
 			$rootScope.$broadcast('utilisateur.utilisateur-deconnecte');
-		},
-		function(data) {
-			
+			// annulation du rafraîchissement
+			if (lthis.timer != null) {
+				$interval.cancel(lthis.timer);
+				// nullifier pour le test dans connaitreEtatUtilisateur()
+				lthis.timer = null;
+			}
 		});
 	};
 
@@ -149,7 +178,12 @@ smartFormApp.controller('BoiteUtilisateurControleur', function ($scope, $rootSco
 		payload = JSON.parse(payload, true);
 
 		return payload;
-	}
+	};
+
+	// destruction du timer si on sort de l'appli
+	$scope.$on("$destroy", function() {
+		$interval.cancel(this.timer);
+	});
 
 	this.initialiserUtilisateurVide();
 	this.connaitreEtatUtilisateur();
