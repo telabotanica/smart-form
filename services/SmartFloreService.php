62742a9f7b6cc29465110e2e3df7df66a7cf8827
kstefanini <stefanini.k@gmail.com>
2018-07-24 16:09:45 +0200

    Fix string to array crap

--- services/SmartFloreService.php
+++ services/SmartFloreService.php
@@ -205,7 +205,7 @@ class SmartFloreService {
 		$jwt = null;
 		if (! function_exists('apache_request_headers')) {
 			function apache_request_headers() {
-				$headers = '';
+				$headers = [];
 				foreach ($_SERVER as $name => $value) {
 					if (substr($name, 0, 5) == 'HTTP_') {
 						$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
